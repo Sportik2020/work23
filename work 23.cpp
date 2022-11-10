fc6525e239ae90668921b2a48d76fbfd0c0d913d
Sportik2020 <114169865+Sportik2020@users.noreply.github.com>
2022-11-10 18:32:31 +0300

    Add files via upload

---
+++ work 23.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+int main()
+{
+/*	const int n = 10;
+	int mas[n];
+	mas[0] = 1;
+	for (int i = 1; i < n; i++) {
+		mas[i] = mas[i - 1] + 2;
+	}
+	for (int i = 0; i < n; i++) {
+		cout << mas[i] << ' ';
+	}*/
+//3
+/*	int a, d;
+	cin >> a >> d;
+	const int n = 10;
+	int mas[n];
+	mas[0] = a;
+	for (int i = 1; i < n; i++) {
+		mas[i] = mas[i - 1] + d;
+	}
+	for (int i = 0; i < n; i++) {
+		cout << mas[i] << ' ';
+	}
+	cout << endl;*/
+//4
+/*	int a, b;
+	cin >> a >> b;
+	const int n = 10;
+	int mas[n];
+	mas[0] = a;
+	for (int i = 1; i < n; i++) {
+		mas[i] = mas[i - 1] * b;
+	}
+	for (int i = 0; i < n; i++) {
+		cout << mas[i] << ' ';
+	}
+	cout << endl;*/
+//5
+/*	const int f = 10;
+	int n[f];
+	n[0] = 1;
+	n[1] = 1;
+		for (int i = 2; i < f; ++i) {
+			n[i] = n[i - 2] + n[i - 1];
+	}
+		for (int i = 0; i < f; ++i) {
+			cout << i << '-' << n[i] << endl;
+	}*/
+//7
+/*	const int n = 10;
+	int mas[n];
+	srand(time(nullptr));
+	for (int i = n - 1; i >= 0; i--) {
+	mas[i] = rand();
+	cout << mas[i] << " ";
+	}*/
+//
+
+
+
+
+
+
+
+	return 0;
+}
